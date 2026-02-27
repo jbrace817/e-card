6fe0687d195e7fc7c5634556488ff79c6fa9a906
James Brace <48393754+jbrace817@users.noreply.github.com>
2026-02-27 11:18:46 -0500

    Enhance comments in `use-store-user.ts` to provide clearer context on user authentication and storage processes, specifying the validation of Clerk's JWT and the behavior for existing users during storage.

--- src/hooks/use-store-user.ts
+++ src/hooks/use-store-user.ts
@@ -5,10 +5,10 @@ import { useEffect, useState } from "react";
 import { api } from "../../convex/_generated/api";
 
 export function useStoreUser() {
-  const { isAuthenticated } = useConvexAuth(); //this is a hook that is used to check if the user is authenticated.
+  const { isAuthenticated } = useConvexAuth(); //this is a hook that is used to check if the user is authenticated. checks if clerks JWT is valid within convex.
   const { user } = useUser(); //this is a hook that is used to get the user object.
   const [userId, setUserId] = useState<string | null>(null);
-  const storeUser = useMutation(api.users.store); //this is a mutation function that is used to store the user in the database.
+  const storeUser = useMutation(api.users.store); // this is a mutation function that is used to store the user in the database. for existing users, this is called once isAuthenticated is true.
 
   useEffect(() => {
     // Don't do anything if the user isn't logged in
